2f8a35909df395053954dcffd834f0f871a968e7
lopiitti <mlop@iki.fi>
2025-10-13 09:05:48 +0300

    Add files via upload
    Adding raw page

---
+++ 2026/tickets.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+	$slug 		= "tickets";
+	$pagename = "Tickets";
+	include "inc/head.inc.php";
+?>
+<body class='text-center hide-background subpage subpage-tickets'>
+
+	<script> var current_navi_item="tickets" </script>
+	<?php include "inc/navigation.inc.php"; ?>
+	<?php include "inc/header.inc.php";	?>
+
+	<script>
+		$(".header-row").append($("#tickets-page-get-ticket"))
+	</script>
+
+	<section class='container hidden'>
+
+		<div class='full-row clearfix lazyload translucent' >
+
+			<section class='text-left grid-container grid-x grid-padding-x grid-padding-y grid-margin-y'>
+
+				<div class='cell large-6 flex-column text-center medium-text-left sideline'>
+
+					<h4>CORPORATE TICKETS</h4>
+
+					<p>The Disobey corporate ticket for the 2025 event. Corporate tickets are aimed for corporations and organizations, and they help us keep regular ticket prices low.</p>
+					<p>Corporate tickets will be available for purchase on Oct 1st 2024 at 09:00 UTC.</p>
+					<p>One corporate ticket costs 300€ (electronic badge included)</p>
+
+					<p>
+						<div class='buy-ticket-button inactive' style="text-decoration: line-through;" >SOLD OUT</div>
+					</p>
+
+				</div>
+
+				<div class='cell large-6 flex-column text-center medium-text-left sideline'>
+
+					<h4>STANDARD TICKETS</h4>
+
+					<p>A standard Disobey ticket grants two day entrance to the event on Feb 14-15th 2025.</p>
+					<p>Standard tickets will be available for purchase on Nov 6th 2024 at 10:00 UTC.</p>
+					<p>One standard ticket costs 49€ (with optional electronic badge +30€; for more info on the badge, see below). You can buy max 3 tickets at a time.</p>
+					<p>
+						<!-- button placeholder keep class buy-ticket-button remove inactive -->
+						<div class='buy-ticket-button inactive' style="text-decoration: line-through;" >SOLD OUT</div>
+					</p>
+				</div>
+
+			</section><!-- Grid end -->
+
+		</div><!-- row end -->
+
+		<div class='full-row clearfix lazyload translucent' >
+			<section class="grid-container cell grid-y cell location" id='puzzle' >
+				<div class="cell">
+					<h1 class="red">Electronic Badge</h1>
+				</div>
+				<div class='cell'>
+					<p>
+					The electronic badge is a physical tool with which you can access the badge challenge and games at Disobey. purchasing it is optional, but if you enjoy badge challenges, you will want to consider it! If you want to check the designs for some previous years, you will find them here: <a href="https://badge.team/docs/badges/disobey-2020/">2020 Badge.team</a>, <a href="https://badge.team/docs/badges/disobey-2019/">2019 Badge.team</a>, <a href="https://github.com/disobeyfi/badge-2019">2019 GitHub</a>, <a href="https://github.com/disobeyfi/badge-2018">2018 GitHub</a>, <a href="https://github.com/disobeyfi/badge-2017">2017 GitHub</a>
+					</p>
+					<p>
+					Important: Please note that you can only buy badges when buying your tickets. You cannot buy them afterwards or at the event - only when buying your ticket.
+					</p>
+				</div>
+			</section>
+		</div><!-- row end -->
+
+		<div class='full-row clearfix divider'>&nbsp;</div>
+		<?php include "inc/contact.inc.php"; ?>
+
+	</section>
+
+	<?php include "inc/footer.inc.php"; ?>
+
+</body>
+</html>
