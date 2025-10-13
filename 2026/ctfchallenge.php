8c21b1f36903c8f1cd05f65abaf685ca00c5d7f5
Tapio Vuorinen <delic@iki.fi>
2025-10-13 09:37:01 +0300

    stilization

--- 2026/ctfchallenge.php
+++ 2026/ctfchallenge.php
@@ -17,22 +17,22 @@
 		<div class='full-row clearfix lazyload translucent competitions' data-src='<?php echo $baseurl; ?>img/lockbox.jpg' style='background-image:url( "" );' >
 				<section class="grid-container cell grid-y cell location" id='workshops' >
 					<div class="cell">
-						<h1>CTF Challenge</h1>
+						<h1>CTF Call for challenges</h1>
 					</div>
 					<div class='cell text-left grid-container grid-x grid-padding-x grid-margin-x grid-padding-y grid-margin-y'>
 
 							<div class='cell large-12 text-center'>
-                                <p>Are you a puzzle crafter, exploit artist, or protocol wizard? Do you have a knack for creating mind-bending challenges that push hackers to their limits? We're inviting <b>up to 10 challenge creators</b> to join the <b>Disobey CTF Challenge Team</b>! If selected, you'll help shape the competition by submitting <b>2–3 original challenges</b>.
-<p>In return, you'll receive:</p>
-<br>- Free entry to the event</br>
-<br>- Some swag</br>
-<br>- •	Eternal glory (or at least some serious bragging rights)</br>
+                                <p>Are you a puzzle crafter, exploit artist, or protocol wizard? Do you have a knack for creating mind-bending challenges that push hackers to their limits? We're inviting <b>up to 10 challenge creators</b> to join the <b>Disobey CTF Challenge Team</b>! If selected, you'll help shape the competition by submitting <b>2-3 original challenges</b>.
+								<p>In return, you'll receive:</p>
+								<br>- Free entry to the event</br>
+								<br>- Some swag</br>
+								<br>- Eternal glory (or at least some serious bragging rights)</br>
 
-<p>To apply, send a short application to <i>games@disobey.fi</i> describing what kind of challenges you’d like to create (e.g., web, crypto, reverse engineering, forensics, etc.) and any relevant experience or ideas you’re excited to explore.</p>
+								<p>To apply, send a short application to <i>games@disobey.fi</i> describing what kind of challenges you'd like to create (e.g., web, crypto, reverse engineering, forensics, etc.) and any relevant experience or ideas you're excited to explore. </p>
 
-<p>Applying is considered a commitment to submit 2–3 challenges. If your challenges are accepted, you're in! </p>
+								<p>Applying is considered a commitment to submit 2-3 challenges. If your challenges are accepted, you're in! </p>
 
-<p>More detailed instructions will be provided to the selected challenge team.</p>
+								<p>More detailed instructions will be provided to the selected challenge team.</p>
 
                             </div>
 
