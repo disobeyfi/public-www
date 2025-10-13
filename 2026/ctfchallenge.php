651481fa25a44880cba2eae43da6d74d738ec16e
Tapio Vuorinen <delic@iki.fi>
2025-10-13 09:40:55 +0300

    more styling

--- 2026/ctfchallenge.php
+++ 2026/ctfchallenge.php
@@ -24,9 +24,11 @@
 							<div class='cell large-12 text-center'>
                                 <p>Are you a puzzle crafter, exploit artist, or protocol wizard? Do you have a knack for creating mind-bending challenges that push hackers to their limits? We're inviting <b>up to 10 challenge creators</b> to join the <b>Disobey CTF Challenge Team</b>! If selected, you'll help shape the competition by submitting <b>2-3 original challenges</b>.
 								<p>In return, you'll receive:</p>
-								<br>- Free entry to the event</br>
-								<br>- Some swag</br>
-								<br>- Eternal glory (or at least some serious bragging rights)</br>
+								<p>
+								<div>- Free entry to the event</div>
+								<div>- Some swag</div>
+								<div>- Eternal glory (or at least some serious bragging rights)</div>
+								</p>
 
 								<p>To apply, send a short application to <i>games@disobey.fi</i> describing what kind of challenges you'd like to create (e.g., web, crypto, reverse engineering, forensics, etc.) and any relevant experience or ideas you're excited to explore. </p>
 
