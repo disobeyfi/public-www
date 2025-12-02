f54ef7f0b26cc2349fc96783989e526d8ee92203
Tapio Vuorinen <delic@iki.fi>
2025-12-02 15:22:46 +0200

    add program to navigation

--- 2026/inc/navigation.inc.php
+++ 2026/inc/navigation.inc.php
@@ -8,12 +8,12 @@
 	$navigation = "<section id='navigation' class='text-center hidden navigation-bar'>
 	<a id='navi-frontpage'    href='{$baseurl}./'><img src='{$baseurl}img/just_logo.png' id='navigation-home-symbol' class='show-for-large'><span class='hide-for-large'>Home</span></a>
 	<a id='navi-hackerpuzzle' href='{$baseurl}HackerPuzzle{$suffix}'>Hacker Puzzle</a>
+	<a id='navi-program'      href='{$baseurl}program{$suffix}'>Program</a>
 	<a id='navi-tickets'      href='{$baseurl}tickets{$suffix}'><i class='far fa-ticket show-for-medium' aria-hidden='true'></i> Get Tickets!</a>
 	<a id='navi-cfc'          href='{$baseurl}ctfchallenge{$suffix}'>Call for challenges</a>
 	<a id='navi-about'        href='{$baseurl}about{$suffix}'>About</a>
 	</section>";
 	/*
-	<a id='navi-program'  href='{$baseurl}program{$suffix}'>Program</a>
 	<a id='navi-practical'  href='{$baseurl}practical{$suffix}'>Practical</a>
 	<a id='navi-venue'  href='{$baseurl}venue{$suffix}'>Venue</a>
 	<a id='navi-competitions'  href='{$baseurl}competitions{$suffix}'>Competitions</a>
