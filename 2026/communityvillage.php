b5828b9169452d87df2115fc9ab2d4c16d4603f9
lopiitti <mlop@iki.fi>
2026-01-29 14:12:41 +0200

    Update communityvillage.php
    bliiblaabloo

--- 2026/communityvillage.php
+++ 2026/communityvillage.php
@@ -139,7 +139,88 @@ $continuous_events = array_filter($continuous_events, function($event) {
                         </div>
                     </div>
                 </section>
-        </div>
+                <section class="grid-container cell grid-y cell location" id='workshops'>
+                    <div class="cell">
+                        <h3>Meet the Villagers</h3>
+                    </div>
+                    <div class='cell text-left grid-container grid-x grid-padding-x grid-margin-x grid-padding-y grid-margin-y'>
+        
+                        <div class='cell large-12 text-center'>
+                            <p>As always, our Community Village is composed by a community of  wonderful organizations and associations. Meet them below (in alphabetical order).</p>
+                        </div>
+        
+                        <div class='cell large-6'>
+                            <h4>Cyber Club Kotka</h4>
+                            <p>Cyber Club Kotka, officially known as Kotkan Kyberturvallisuusinsinööriopiskelijat ry, is the student association for cybersecurity students at XAMK. Our goal is to help students find friends and get to know each other at the very beginning of their studies, support networking within the cybersecurity field, and offer members shared activities and events related to cybersecurity as well as activities beyond the field.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Hacklab</h4>
+                            <p>Do you need tools, place or community to make physical things? Local Hacklab might be your solution. At Disobey we have small electronics workbenches, some 3D printing and all kinds of projects our members have made.</p>
+                        </div>
+                            <div class='cell large-6'>
+                            <h4>Halko - Hacklab Kuopio ry</h4>
+                            <p>Halko - Hacklab Kuopio ry is a non-profit communal workspace / makerspace in Kuopio, Finland. We provide spaces, equipment and community for many kinds of projects involving both tech and art. Everyone 18+ is welcome to come visit us on our twice per week open days and check out the premises and meet people. Experience is not required, Halko is a great space to get inspiration, learn new stuff, work on projects and have fun. https://halko.fi</p>
+                        </div>
+                            <div class='cell large-6'>
+                            <h4>HelSec Ry</h4>
+                            <p>Cybersecurity group by the community, for the community. Founded in 2018, a non-profit organization HelSec currently organizes regular meetups, where security professionals, enthusiasts and other like-minded people can meet and network in the Helsinki region.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>H-T8 Hacking Club</h4>
+                            <p>H‑T8 is a student‑driven hacking club created by students, for students. Our mission is to open the doors to the exciting world of cybersecurity and help newcomers take their first steps as ethical hackers. We organize excursions, CTF competitions and student events where members can learn practical skills and network. Whether you’re an absolute beginner or already exploring advanced topics, H‑T8 offers a supportive community where curiosity and experimentation are encouraged.</p>
+
+                            <p>For many, we are the first real connection to cybersecurity, and we take pride in making that introduction fun, inclusive, and inspiring. We offer content for all skill levels, from basic introductions to advanced activities. Our experienced members also form a competitive CTF team that participates in contests throughout the year. If you’re a student or someone who enjoys supporting students in this field, come join us and be part of the H‑T8 community! https//ht8.fi/</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>KuoSec ry</h4>
+                            <p>KuoSec ry is part of the CitySec family. It is an infosec community located at Kuopio, Finland. Our purpose is to organize meetups and get-togethers focused on training and networking. Anyone interested in the Infosec field is welcome to join us, meet likeminded people and hopefully learn something new. https://kuosec.fi</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>KymiSec ry</h4>
+                            <p>KymiSec ry is part of the CitySec family. We are an information security community based in the Kymenlaakso region of Finland. Our goal is to organize infosec-related meetups and events focused on learning and networking. KymiSec works in cooperation with Cyber Club Kotka (CCK). Anyone interested in information security and cybersecurity is welcome to join us, meet like-minded people, and develop their skills. Find us at https://kymisec.fi</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>LahtiSec</h4>
+                            <p>Lahti Cyber Security Society (LahtiSec) is the CitySec chapter in Lahti, Finland. We host meetups three times a year, grow the local security community, and make cyber security easier to approach whether you’re a pro, a student, or just curious. Visit our Community Village stand to meet the crew, hear what's going on (including KyberLahti 2026), and catch a little “Business City” energy!</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Lockpicking Village</h4>
+                            <p>Four experienced/professional lockpickers teaching and showing how locks and lockpicking works. We aim to introduce the basics of physical security to everyone, so that together the security concious minds would improve total security.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>PoroSec</h4>
+                            <p>The InfoSec Community of Northern Finland. The Information Security community from the northern part of Finland (mainly Rovaniemi) welcomes anyone interested in information security or eager to learn something new. We organize meetups and events for infosec enthusiasts.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Robot Uprising ry</h4>
+                            <p>Robot Uprising is a volunteer-led community that shares a common love for robots and making cool stuff happen. We organize hands-on robotics events combining gaming, storytelling, and tech challenges.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>SaimaaSec</h4>
+                            <p>Information security community from Lappeenranta. We organise meetups and community events on a regular basis. We welcome anyone interested in information security, whether you are a student, professional or just curious about infosec.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Skrolli</h4>
+                            <p>At the Skrolli booth you can try out older and more unique home computers that you may recognize from the pages of the magazines. You can also let us know if you'd be interested in an international edition of Skrolli and of course subscribe Skrolli for 2026 or purchase older issues. Join the conversation on Skrolli’s reader channels (Discord, Matrix, IRC) at skrolli.fi/lukijakanavat/</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>SRAL / Radio amateurs</h4>
+                            <p>Amateur radio is where curiosity meets communication — visit the SRAL booth to discover how radio enthusiasts build DIY gear, explore signals, and connect with the world in surprising ways.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Testausserveri</h4>
+                            <p>Testausserveri is an open community for young and young minded people interested in programming, ethical hacking and tech in general. This Disobey you can try your hardest with our CTF challenges and come meet us along with some guest stars from the Finnish European Cybersecurity Competition team.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>TurkuSec</h4>
+                            <p>Mother of CitySecs. TurkuSec ry is a nonprofit organization of information security professionals, practitioners and enthusiasts. Visit us on DisObey Community Village to chat about citysecs, cyber stuff and life in general, try LLM hacking challenges, and some ad-hoc challenges.</p>
+                        </div>
+                        <div class='cell large-6'>
+                            <h4>Women4Cyber Finland</h4>
+                            <p>Women4Cyber Finland is a non profit association dedicated to advancing women’s careers in cybersecurity, thus increasing diversity and equalizing the existing gender imbalance. As Finland’s official chapter within a European network of over 30 chapters, we promote equal opportunities, raise awareness, and strengthen the national cyber talent pool. We contribute to public dialogue, host networking and learning events, and provide support and up to date insights in close cooperation with our partners.</p>
+                       </div>
+                    </section>
+                </div>
 
         <div class='full-row clearfix divider'>&nbsp;</div>
         <?php include "inc/contact.inc.php"; ?>
